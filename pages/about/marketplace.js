a00ceaee4e281751bd4d775d05c589ccdf2d62b3
k8pai <thek8pai@gmail.com>
2022-10-10 08:03:49 +0530

    updated marketplace section

--- pages/about/marketplace.js
+++ pages/about/marketplace.js
@@ -3,8 +3,27 @@ import Link from "next/link";
 import Header from "../../components/Header"
 import Footer from "../../components/Footer";
 import Head from "next/head";
+import { motion } from "framer-motion";
 
 export default function marketplace() {
+
+    const container = {
+        hidden:{ opacity: 0},
+        visible:{ opacity: 1, transition:{ staggerChildren: .1,}}
+    }
+    const items = {
+        hidden:{ opacity: 0, y: 20,},
+        visible:{ opacity: 1, y:0, transition:{ type: "spring", bounce: .5, stiffness:100,}, },
+    }
+
+    const ulList = {
+        hidden:{ opacity: 0},
+        visible:{ opacity: 1, transition:{ staggerChildren: .07, delayChildren: .5,}}
+    }
+    const ulItems = {
+        hidden:{ opacity: 0, y: 20,},
+        visible:{ opacity: 1, y:0, transition:{ type: "spring", bounce: .5, stiffness:100,}, },
+    }
     return (
         <>
             <Head>
@@ -36,36 +55,37 @@ export default function marketplace() {
                 <main className="relative min-h-screen h-full flex flex-col justify-between">
                     <Header />
                     <section className="TeamsSection -z-0 relative w-[100%] min-h-full flex-col justify-center items-center mx-auto">
-                        <div className="min-w-screen w-full flex md:flex-col sm:flex-col xsm:flex-col">
-                            <div className="basis-2/3 select-none w-[90%] mx-auto px-[50px] py-[100px]">
-                                <div className="main-team-header font-fjalla tracking-widest text-left">
-                                    <h1 className="text-6xl text-slate-300 uppercase leading-normal font-bold tracking-wider sm:text-4xl xsm:text-3xl">Streetwear Marketplace</h1>
-                                    <h1 className="text-2xl pt-[25px] text-slate-300 capitalize leading-relaxed font-normal sm:text-xl xsm:text-lg">We&apos;re building a Streetwear Marketplace. A totally different concept, that will make us unique from other streetwear marketplaces out there.  Our marketplace will be community-based and what makes us different is the ranking system we&apos;ll be having for the sellers in our marketplace!</h1>
-                                    <h1 className="text-2xl pt-[25px] text-slate-300 capitalize leading-relaxed font-normal sm:text-xl xsm:text-lg">Our ranking system makes our marketplace more community-driven, and that&apos;s an important part, at least in streetwear culture. And this way you&apos;ll be able to see who&apos;s selling you the sneakers and his previous sales. And you can be assured that the chances of you getting fakes are lesser! And if you want to make it close to NIL! - We got our monthly subscription-based services.</h1>
-                                    <h1 className="text-2xl pt-[25px] text-slate-300 capitalize leading-relaxed font-normal sm:text-xl xsm:text-lg">Our marketplace will be proving monthly subscription-based services too. Our holders can get access to it by staking and they&apos;ll be getting in-house lottery tickets which they can use on different raffles we&apos;ll be hosting and for other utilities of our marketplace.</h1>
-
-                                    <h1 className="text-2xl pt-[25px] text-slate-300 capitalize leading-relaxed font-normal sm:text-xl xsm:text-lg">And this how we plan to conquer the 30B Dollar Streetwear Reselling Industry!</h1>
-                                </div>
+                        <div className="min-w-screen w-full flex items-start md:flex-col sm:flex-col xsm:flex-col">
+                            <div className="basis-2/3 select-none w-[90%] mx-auto px-[50px] mt-[100px]">
+                                <motion.div variants={container} initial={"hidden"} animate={"visible"} className="main-team-header font-fjalla tracking-widest text-left">
+                                    <motion.h1 variants={items} className="text-6xl text-slate-300 uppercase leading-normal font-bold tracking-wider sm:text-4xl xsm:text-3xl">Streetwear Marketplace</motion.h1>
+                                    <motion.h1 variants={items} className="text-2xl pt-[25px] text-slate-300 capitalize leading-relaxed font-normal sm:text-xl xsm:text-lg">We&apos;re building a Streetwear Marketplace. A totally different concept, that will make us unique from other streetwear marketplaces out there.  Our marketplace will be community-based and what makes us different is the ranking system we&apos;ll be having for the sellers in our marketplace!</motion.h1>
+                                    <motion.h1 variants={items} className="text-2xl pt-[25px] text-slate-300 capitalize leading-relaxed font-normal sm:text-xl xsm:text-lg">Our ranking system makes our marketplace more community-driven, and that&apos;s an important part, at least in streetwear culture. And this way you&apos;ll be able to see who&apos;s selling you the sneakers and his previous sales. And you can be assured that the chances of you getting fakes are lesser! And if you want to make it close to NIL! - We got our monthly subscription-based services.</motion.h1>
+                                    <motion.h1 variants={items} className="text-2xl pt-[25px] text-slate-300 capitalize leading-relaxed font-normal sm:text-xl xsm:text-lg">Our marketplace will be proving monthly subscription-based services too. Our holders can get access to it by staking and they&apos;ll be getting in-house lottery tickets which they can use on different raffles we&apos;ll be hosting and for other utilities of our marketplace.</motion.h1>
+                                    <motion.h1 variants={items} className="text-2xl pt-[25px] text-slate-300 capitalize leading-relaxed font-normal sm:text-xl xsm:text-lg">And this how we plan to conquer the 30B Dollar Streetwear Reselling Industry!</motion.h1>
+                                </motion.div>
                             </div>
-                            <div className="basis-1/3 min-h-full h-fit w-[90%] mx-auto px-[50px] py-[100px]">
-                                <Image
-                                    src={"/images/worfWithMoon.png"}
-                                    width="500"
-                                    height="500"
-                                    quality={100}
-                                    alt="airdrop picture."
-                                    priority
-                                />
-                                <h1 className="text-2xl text-slate-300 capitalize leading-relaxed font-normal sm:text-xl xsm:text-lg"> Our premium subscription will include -</h1>
-                                <ul className="list-disc text-left">
-                                    <li className="text-xl leading-relaxed text-[#87F6FF] capitalize font-normal sm:xl xsm:text-lg"> Buy/Sell Streetwear and Sneakers using CRYPTO</li>
-                                    <li className="text-xl leading-relaxed text-[#87F6FF] capitalize font-normal sm:xl xsm:text-lg"> Sneaker raffles [Holders will get multiple free enties]</li>
-                                    <li className="text-xl leading-relaxed text-[#87F6FF] capitalize font-normal sm:xl xsm:text-lg"> Legit check/middleman service.</li>
-                                    <li className="text-xl leading-relaxed text-[#87F6FF] capitalize font-normal sm:xl xsm:text-lg"> Holders/Subscribers will be able to consign their sneakers on our marketplace.</li>
-                                    <li className="text-xl leading-relaxed text-[#87F6FF] capitalize font-normal sm:xl xsm:text-lg"> Sourcing service.</li>
-                                    <li className="text-xl leading-relaxed text-[#87F6FF] capitalize font-normal sm:xl xsm:text-lg"> Restocking BOT Alerts, Sneaker Drops Alerts on our discord.</li>
-                                    <li className="text-xl leading-relaxed text-[#87F6FF] capitalize font-normal sm:xl xsm:text-lg"> Reshipping across the USA, Canada, UK, UAE, and India! (We will be expanding this service to other countries too!)</li>
-                                </ul>
+                            <div className="basis-1/3 min-h-full h-fit w-[90%] mx-auto px-[50px] mt-[65px]">
+                                <motion.div initial={{opacity:0, scale: .80}} animate={{opacity:1, scale: 1, transition:{ delay:.1,}}}>
+                                    <Image
+                                        src={"/images/worfWithMoon.png"}
+                                        width="500"
+                                        height="500"
+                                        quality={100}
+                                        alt="airdrop picture."
+                                        priority
+                                    />
+                                </motion.div>
+                                <motion.h1 initial={{opacity:0, scale: .98}} animate={{opacity:1, scale: 1, transition:{ delay:.33,}}} className="text-2xl text-slate-300 capitalize leading-relaxed font-normal sm:text-xl xsm:text-lg"> Our premium subscription will include -</motion.h1>
+                                <motion.ul variants={ulList} initial={"hidden"} animate={"visible"} className="list-disc text-left">
+                                    <motion.li variants={ulItems} className="text-xl leading-relaxed text-[#87F6FF] capitalize font-normal sm:xl xsm:text-lg"> Buy/Sell Streetwear and Sneakers using CRYPTO</motion.li>
+                                    <motion.li variants={ulItems} className="text-xl leading-relaxed text-[#87F6FF] capitalize font-normal sm:xl xsm:text-lg"> Sneaker raffles [Holders will get multiple free enties]</motion.li>
+                                    <motion.li variants={ulItems} className="text-xl leading-relaxed text-[#87F6FF] capitalize font-normal sm:xl xsm:text-lg"> Legit check/middleman service.</motion.li>
+                                    <motion.li variants={ulItems} className="text-xl leading-relaxed text-[#87F6FF] capitalize font-normal sm:xl xsm:text-lg"> Holders/Subscribers will be able to consign their sneakers on our marketplace.</motion.li>
+                                    <motion.li variants={ulItems} className="text-xl leading-relaxed text-[#87F6FF] capitalize font-normal sm:xl xsm:text-lg"> Sourcing service.</motion.li>
+                                    <motion.li variants={ulItems} className="text-xl leading-relaxed text-[#87F6FF] capitalize font-normal sm:xl xsm:text-lg"> Restocking BOT Alerts, Sneaker Drops Alerts on our discord.</motion.li>
+                                    <motion.li variants={ulItems} className="text-xl leading-relaxed text-[#87F6FF] capitalize font-normal sm:xl xsm:text-lg"> Reshipping across the USA, Canada, UK, UAE, and India! (We will be expanding this service to other countries too!)</motion.li>
+                                </motion.ul>
                             </div>
                         </div>
                     </section>
